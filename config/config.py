95dbe764627bfd691e837460d722e538d2bbeda6
Altair000 <ains12403@gmail.com>
2024-08-10 00:30:52 -0400

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-Host = 'http://bm47cpet7wmgfacjmrc0-mysql.services.clever-cloud.com'
+Host = 'bm47cpet7wmgfacjmrc0-mysql.services.clever-cloud.com'
 User = 'uu9w727xwga1klfg'
 Password= 'A7eH0T6ITxtjmU1MSQd9'
 Database = 'bm47cpet7wmgfacjmrc0'
