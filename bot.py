ac9371f6044e1979672ef0033b5f9ab7381ba614
Altair000 <ains12403@gmail.com>
2024-08-14 03:54:14 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,18 @@
 import telebot
+from flask import Flask, request
 from handlers.command_handlers import register_command_handlers
 from handlers.callback_handlers import register_callback_handlers
 from utils.bot_config import *
 from admin.admin_cmds import register_admin_command_handlers
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '', 200
 
 # Registra los manejadores de comandos y callbacks
 register_command_handlers(bot)
@@ -11,5 +21,4 @@ register_admin_command_handlers(bot)
 
 # Main
 if __name__ == '__main__':
-    print('INICIANDO...')
-    bot.infinity_polling()
+    app.run()
