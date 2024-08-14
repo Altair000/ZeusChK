0374a18216a1e3c47d6ea3bdb8a0cf0573792b76
Altair000 <ains12403@gmail.com>
2024-08-14 12:33:09 -0400

    Update shopify.py

--- gates/shopify.py
+++ gates/shopify.py
@@ -71,7 +71,7 @@ def shopify(cc, mes, ano, cvv, message):
         for i in range(10):
             bot.edit_message_text(chat_id=message.chat.id,
                                   text = f'''
-                                  • VERIFICANDO CC: •｛i*10｝%''',
+                                  • VERIFICANDO CC: •{i * 10}%''',
                                   reply_markup=None,
                                   message_id=sent_message.message_id,
                                   parse_mode="HTML"
