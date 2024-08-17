f6f27ea0a78d884486fa33a9c38c8d573c36260c
Altair000 <ains12403@gmail.com>
2024-08-17 10:12:19 -0400

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -9,12 +9,67 @@ from utils.bot_config import *
 from admin.admin_cmds import register_admin_command_handlers
 from admin.admin_cmds import *
 from gates.stripe import *
+from utils.proxies import *
 
 # Configure BugSnag
 bugsnag.configure(
   api_key = "d2055e6398e4602fb88c70a77cf896ac",
   project_root = "https://zeuschk-64ea0cb25362.herokuapp.com/",
 )
+
+@bot.message_handler(content_types=['document'])
+def handle_document(message):
+    if not is_owner(message.from_user.id):
+        bot.send_message(message.chat.id, "No tienes permiso para usar este comando.")
+        return
+    bot.send_message(message.chat.id, "ACCESO CONCEDIDO.")
+    if message.document.file_name == 'proxies.txt':
+        file_path = bot.get_file_path(message.document.file_id)
+        proxies_list = update_proxies_list(file_path)
+
+        sent_message = bot.send_message(message.chat.id, "Comenzando...")
+        for i in range(10):
+            bot.edit_message_text(chat_id=message.chat.id,
+                                  text = f'''
+                                  • ADQUIRIENDO PROXIES DE FILE: •{i * 10}%''',
+                                  reply_markup=None,
+                                  message_id=sent_message.message_id,
+                                  parse_mode="HTML"
+                                  )
+            time.sleep(1)
+        
+        functional_proxies, functional_count, removed_proxies, total_proxies = filter_and_update_proxies(proxies_list)
+
+        sent_message_1 = bot.send_message(message.chat.id, "Comenzando Verificacion de Proxies...")
+        for i in range(10):
+            bot.edit_message_text(chat_id=message.chat.id,
+                                  text = f'''
+                                  • VERIFICANDO PROXIES: •{i * 10}%''',
+                                  reply_markup=None,
+                                  message_id=sent_message_1.message_id,
+                                  parse_mode="HTML"
+                                  )
+            time.sleep(1)
+        
+        response_message = (
+            f"Total de proxies: {total_proxies}\n"
+            f"Proxies funcionales: {functional_count}\n"
+            f"Proxies eliminados: {removed_proxies}\n"
+            "¿Estás satisfecho con la respuesta? (sí/no)"
+        )
+        bot.send_message(message.chat.id, response_message)
+
+        @bot.message_handler(func=lambda m: m.chat.id == message.chat.id)
+        def satisfaction_response(satisfaction_message):
+            if satisfaction_message.text.lower() == 'sí':
+                bot.send_message(message.chat.id, "PROXIES ACTUALIZADOS")
+            elif satisfaction_message.text.lower() == 'no':
+                bot.send_message(message.chat.id, "Por favor, envía un nuevo archivo de proxies.")
+                bot.register_next_step_handler(satisfaction_message, handle_document)
+            else:
+                bot.send_message(message.chat.id, "Por favor, responde con 'sí' o 'no'.")
+    else:
+	    bot.send_message(message.chat.id, "Por favor, envía un archivo llamado 'proxies.txt'.")
 
 app = Flask(__name__)
 handle_exceptions(app)
