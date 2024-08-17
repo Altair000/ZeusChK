170de4b114a14be2a315cca9addaef0a98a2c7e0
Altair000 <ains12403@gmail.com>
2024-08-17 01:51:12 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from handlers.command_handlers import register_command_handlers
 from handlers.callback_handlers import register_callback_handlers
 from utils.bot_config import *
 from admin.admin_cmds import register_admin_command_handlers
+from admin.admin_cmds import *
 from gates.stripe import *
 
 app = Flask(__name__)
