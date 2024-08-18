a7a3a0f03a8f083883fd8f926e1b7b75e5ca29c8
Altair000 <ains12403@gmail.com>
2024-08-17 21:16:22 -0400

    Actualizar proxies.py

--- utils/proxies.py
+++ utils/proxies.py
@@ -1,6 +1,6 @@
 import requests
 from utils.bot_config import *
-from admin.admin_cmds import *
+from admin.admin_cmds import handle_document, satisfaction_response
 
 def update_proxies_list(file_path):
     proxies_list = {}
