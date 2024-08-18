712f841144af9f94dbb890596bc4c082b1bfb866
Altair000 <ains12403@gmail.com>
2024-08-18 19:10:32 -0400

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,6 @@ from handlers.command_handlers import register_command_handlers
 from handlers.callback_handlers import register_callback_handlers
 from utils.bot_config import *
 from admin.admin_cmds import register_admin_command_handlers
-from admin.admin_cmds import *
-from gates.stripe import *
 from utils.proxies import *
 from database.db import *
 
